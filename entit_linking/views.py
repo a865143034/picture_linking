02272202fa715b5029bd092b8400808019f1a5f7
wangkun <wangkun@adapt.seiee.sjtu.edu.cn>
2019-03-21 16:59:05 +0800

    2019.3.21

--- entit_linking/views.py
+++ entit_linking/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 import json
 import gensim
 import nltk
-
+from lxml import etree
 
 import requests
 from bs4 import BeautifulSoup
@@ -32,9 +32,19 @@ def get_img(html):
     except:
         return ""
 
+def parse_(html):
+    try:
+        html = etree.HTML(html)
+        img= html.xpath('//*[@id="file"]/a/img/@src')[0]
+        img="https:"+img
+        return img
+    except:
+        return 0
+
+
 def func(url):
     htm=getHTMLText(url)
-    ans=get_img(htm)
+    ans=parse_(htm)
     return ans
 
 
@@ -75,6 +85,67 @@ def trim(text):
 
 
 
+import queue
+import threading
+import time
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, q):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.q = q
+    def run(self):
+        self.process_data(self.q)
+    @staticmethod
+    def process_data(q):
+        while not exitFlag:
+            queueLock.acquire()
+            if not workQueue.empty():
+                data = q.get()
+                queueLock.release()
+                tmp_ans=func(data[1])
+                ans_queue.put((data[0],tmp_ans))
+            else:
+                queueLock.release()
+            time.sleep(0.1)
+
+def thread_caw(urlList):
+    global exitFlag
+    global workQueue
+    global queueLock
+    global ans_queue
+    exitFlag=0
+    workQueue=queue.Queue()
+    queueLock=threading.Lock()
+    ans_queue=queue.Queue()
+    threadnum=20
+    threads = []
+
+    for threadID in range(threadnum):
+        thread = myThread(threadID, workQueue)
+        thread.start()
+        threads.append(thread)
+
+    queueLock.acquire()
+    for word in urlList:
+        workQueue.put(word)
+    queueLock.release()
+
+    while not workQueue.empty():pass
+
+    exitFlag = 1
+    for t in threads:
+        t.join()
+
+
+def get_img_dic(url_list):
+    thread_caw(url_list)
+    dic_={}
+    while not ans_queue.empty():
+        data=ans_queue.get()
+        dic_[data[0]]=data[1]
+    return dic_
+
 def linking(request):
     f=open('output_enwiki.txt','r')
     m={}
@@ -88,10 +159,16 @@ def linking(request):
         ans=""
         text=request.POST['text']
         dic=trim(text)
+        nxt_list=[]
         for obj in dic:
             wrd=obj.lower()
             if wrd in m:
-               t1=func(m[wrd])
+                nxt_list.append((wrd,m[wrd]))
+        dic_=get_img_dic(nxt_list)
+        for obj in dic:
+            wrd=obj.lower()
+            if wrd in m:
+               t1=dic_[wrd]
                if not t1:
                   ans+=obj+" "
                   continue
@@ -101,7 +178,7 @@ def linking(request):
                ans+=obj+" "
         return HttpResponse(
             json.dumps({
-		"ans":ans,
+                "ans": ans,
             }))
 
 
